dee73c582866b5e06d4c878b5d7b9e0ec21efe63
Bri in The Sky <brianh6854@googlemail.com>
2016-03-22 17:41:22 +0000

    Version update to 3.0.4

--- src/icecrystals/version.cr
+++ src/icecrystals/version.cr
@@ -4,8 +4,8 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY = 3
-    CODENAME = "Icecrystals"
+    TINY = 4
+    CODENAME = "Crystaline Structures"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
 

a33a4f74c78bc6af229a2955b0d56ac71a13ec58
Bri in The Sky <brianh6854@googlemail.com>
2016-07-01 10:33:54 +0100

    Version increment / new tag

--- src/icecrystals/version.cr
+++ src/icecrystals/version.cr
@@ -4,8 +4,8 @@ module Icersplicer
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 1
-    TINY = 4
-    CODENAME = "Emerald"
+    TINY = 6
+    CODENAME = "Emerald Fields"
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
 
